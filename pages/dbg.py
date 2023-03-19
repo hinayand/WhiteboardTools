dbac379861c5c3906749523d19728f50d46b0db9
m0_52933302 <3093812306@qq.com>
2023-03-19 12:17:42 +0800

    Fix dbg page: timer never release

--- pages/dbg.py
+++ pages/dbg.py
@@ -5,20 +5,27 @@ from threading import Timer
 def dbg(page: ft.Page):
     local_view = ft.Markdown(f"{locals()}", selectable=True)
     global_view = ft.Markdown(f"{globals()}", selectable=True)
+    timer = None
     
     view = ft.View("/dbg", controls=[
         ft.AppBar(title=ft.Text("Debug Page")),
         ft.ListView([
             local_view,
             global_view
-        ], expand=True, spacing=10)
+        ], expand=True, spacing=10),
+        ft.ElevatedButton("停止刷新", on_click=lambda _: stop())
     ])
 
     def refresh():
         local_view.value = f"{locals()}"
         global_view.value = f"{globals()}"
+        nonlocal timer
         page.update()
-        Timer(0.1, refresh).start()
+        timer = Timer(0.1, refresh)
+        timer.start()
+
+    def stop():
+        timer.cancel()
 
     refresh()
 
